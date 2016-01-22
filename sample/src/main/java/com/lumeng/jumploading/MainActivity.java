8fbd73a228efc3df28a8e8b65226757ac765f2ed
LuMeng <jiahehz@gmail.com>
2016-01-22 11:17:43 +0800

    add About

--- sample/src/main/java/com/lumeng/jumploading/MainActivity.java
+++ sample/src/main/java/com/lumeng/jumploading/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lumeng.jumploading;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lumeng.jump_loading.LoadingView;
 
@@ -45,4 +48,18 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about){
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
